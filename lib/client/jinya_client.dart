9911858cb3d4d8dfae31967bb56d90dd0269f57e
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2022-11-08 22:48:58 +0100

    Format code

--- lib/client/jinya_client.dart
+++ lib/client/jinya_client.dart
@@ -37,7 +37,7 @@ class _JinyaResponse {
       throw NotEnoughPermissionsException();
     } else if (response.statusCode == io.HttpStatus.conflict) {
       throw ConflictException();
-    }else if (response.statusCode < 200 || response.statusCode>299 ){
+    } else if (response.statusCode < 200 || response.statusCode > 299) {
       throw io.HttpException(response.body);
     }
 
